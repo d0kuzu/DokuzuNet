94f9cf2646b5e78217b263a54e4d04cdcbf1ba03
d0kuzu <dkobdabaev@mail.ru>
2025-11-10 15:33:49 +0500

    IConnection & UpdConnection

---
+++ DokuzuNet/Core/Connection/IConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DokuzuNet.Core.Connection
+{
+    public interface IConnection : IDisposable
+    {
+        IPEndPoint EndPoint { get; }
+        bool IsConnected { get; }
+        DateTime LastReceived { get; }
+        DateTime LastSent { get; }
+
+        ValueTask SendAsync(ReadOnlyMemory<byte> data, CancellationToken ct = default);
+        void Disconnect();
+        void UpdateLastReceived();
+        void UpdateLastSent();
+    }
+}
---
+++ DokuzuNet/Core/Connection/UdpConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DokuzuNet.Core.Connection
+{
+    public class UdpConnection : IConnection
+    {
+        private readonly UdpClient _udpClient;
+        private readonly IPEndPoint _endPoint;
+        private bool _isConnected = true;
+
+        public IPEndPoint EndPoint => _endPoint;
+        public bool IsConnected => _isConnected;
+        public DateTime LastReceived { get; private set; }
+        public DateTime LastSent { get; private set; }
+
+        public UdpConnection(UdpClient udpClient, IPEndPoint endPoint)
+        {
+            _udpClient = udpClient ?? throw new ArgumentNullException(nameof(udpClient));
+            _endPoint = endPoint ?? throw new ArgumentNullException(nameof(endPoint));
+            LastReceived = LastSent = DateTime.UtcNow;
+        }
+
+        public async ValueTask SendAsync(ReadOnlyMemory<byte> data, CancellationToken ct = default)
+        {
+            if (!_isConnected) throw new ObjectDisposedException(nameof(UdpConnection));
+
+            try
+            {
+                await _udpClient.SendAsync(data, _endPoint, ct).ConfigureAwait(false);
+                UpdateLastSent();
+            }
+            catch (Exception ex) when (ex is SocketException or ObjectDisposedException)
+            {
+                Disconnect();
+                throw;
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (!_isConnected) return;
+            _isConnected = false;
+        }
+
+        public void UpdateLastReceived() => LastReceived = DateTime.UtcNow;
+        public void UpdateLastSent() => LastSent = DateTime.UtcNow;
+
+        public void Dispose() => Disconnect();
+
+        public override string ToString() => $"{_endPoint} (Conn: {_isConnected})";
+    }
+}
